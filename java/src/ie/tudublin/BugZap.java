1a6aca84b37a288e0ba3fc9102dd565fb5ee4496
C22726225 <C22726225@mytudublin.ie>
2024-01-31 16:48:46 +0000

    Drew a bug using lines and triangle functions

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -2,15 +2,34 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class BugZap extends PApplet {
-	public void settings() {
+public class BugZap extends PApplet 
+{
+	public void settings() 
+	{
 		size(1000, 1000);
 	}
 
-	public void setup() {
+	public void setup() 
+	{
 		
 	}
 
+	public BugZap()
+	{
+	
+	}
+
+	public float bugX = 500.0f;
+	public float bugY = 15.0f;
+	public float bugWidth = 30.0f;
+
+	public void drawBug(float x, float y, float w)
+	{
+		triangle(490, 25, 500, 10, 510, 25);
+		line(493, 25, 490, 30);
+		line(507, 25, 510, 30);
+		line(500, 25, 500, 30);
+	}
 
 	public void keyPressed()
 	{
@@ -30,6 +49,9 @@ public class BugZap extends PApplet {
 
 	int gameMode = 0;
 
-	public void draw() {
+	public void draw() 
+	{
+		// float move_bug_x = random(480, 520);
+		drawBug(bugX, bugY, bugWidth);
 	}
 }
